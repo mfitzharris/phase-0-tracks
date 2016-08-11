1c0f67c7eb47d25f680a292132d7fc1978a82d88
mfitzharris <molly.fitzharris@gmail.com>
2016-08-11 11:44:05 -0500

    Pseudocode a program to allow interior designer to enter details of a given client

---
+++ ruby/hashes.rb
@@ -0,0 +1,23 @@
+#initialize empty hash
+#ask for clients name - 
+  #string, keep as is
+#do they have kids - 
+  #string, convert to boolean (if/else)
+#number of client's children - 
+  #string, convert to integer or nil if left blank
+#desired decor theme - 
+  #string, keep as is
+#restrictions - 
+  #string, keep as is
+#budget
+  #string, convert to integer
+
+#print hash to screen
+
+#ask user if they need to update a key
+#if "none" skip
+#else 
+  #ask for key they want to update
+    #turn string into symbol
+  #ask for new value
+  #somehow handle converting data > loop back to previous code?? test things out

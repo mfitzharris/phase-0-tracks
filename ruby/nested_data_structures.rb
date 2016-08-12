56d3b07aff18b3d5e8ec85b9f7f3d36e9e2aae2c
mfitzharris <molly.fitzharris@gmail.com>
2016-08-12 15:16:08 -0500

    Add code to print a few pieces of deeply nested data

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -6,7 +6,8 @@ classroom = {
     },
     teacher: {
       number_of: 1, 
-      names: ["bloop bleep"]
+      names: ["bloop bleep"],
+      species: "robot"
     }
   },
   class_pet: {
@@ -22,11 +23,34 @@ classroom = {
     snacks: 45,
   },
   furniture: {
-    student_desks: 10,
-    teacher_desks: 1,
+    desks: {
+      student_desks: 10,
+      teacher_desks: 1,
+      },
     book_shelves: 4,
     white_board: 2,
   }
 }
+
+#will print an alphabetized list of the students
+p classroom[:people][:students][:names].sort!
+
+# print the number of each supply in the classroom
+classroom[:supplies].each do |supply, number|
+  puts "there are #{number} #{supply} in the classroom"
+end
+
+#print some information about the class pet
+p "The class's pet #{classroom[:class_pet][:species]}'s name is #{classroom[:class_pet][:name].capitalize}. They are #{classroom[:class_pet][:age]} years old."
+
+#print the total number of desks
+p "The total number of desks in the classroom is: #{classroom[:furniture][:desks][:student_desks]+classroom[:furniture][:desks][:teacher_desks]}"
+
+#print some info about the teacher
+p "The teacher is #{classroom[:people][:teacher][:names][0]}, they are a #{classroom[:people][:teacher][:species]}."
+
+
+
+
 
 

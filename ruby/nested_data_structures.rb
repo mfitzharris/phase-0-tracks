0e72fa6f39b0a91dba9d9f773bbebe272dd8f1a6
mfitzharris <molly.fitzharris@gmail.com>
2016-08-12 14:37:43 -0500

    Create nested data structure representing a classroom

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,32 @@
+classroom = {
+  people: {
+    students: {
+      number_of: 10,
+      names: ["bob", "susy", "goose", "fred", "louis", "tiffany", "caitlin", "matt", "george", "eloise"],
+    },
+    teacher: {
+      number_of: 1, 
+      names: ["bloop bleep"]
+    }
+  },
+  class_pet: {
+    name: "snuggles",
+    species: "bunny",
+    age: 1
+  },
+  supplies: {
+    pencils: 50,
+    notebooks: 20,
+    crayons: 500,
+    books: 60,
+    snacks: 45,
+  },
+  furniture: {
+    student_desks: 10,
+    teacher_desks: 1,
+    book_shelves: 4,
+    white_board: 2,
+  }
+}
+
+

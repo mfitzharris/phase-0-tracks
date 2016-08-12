26160ce2fead2aee009e990c3043e5c9539b92e4
mfitzharris <molly.fitzharris@gmail.com>
2016-08-12 17:17:15 -0500

    Create program to create aliases

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,12 +1,13 @@
-#write a method to take a spys real name
-#create a fake name
+#write a method to take a spy's real name
+# and create a fake name
   #swap first and last name
   #aeiou --> eioua
   #bcdfghjklmnpqrstvwxyz --> cdfghjklmnpqrstvwxyzb
 #abcdefghijklmnopqrstuvwxyz
 
+#################################################
+
 # run .downcase method 
-# separate the first and last name into array
 # separate letters into array
 # when " " do nothing
 # change aeiou --> eioua
@@ -22,5 +23,51 @@
   # when t change to v
   # when z change to b
   # otherwise, run .next method
+# run .join
+# split at " " and switch first and last name
 # run .join on new name
 # run .capitalize! on new name
+
+##############################################
+
+def make_alias(name)
+  name.downcase!
+  name_letters = name.split('')
+  name_letters.map! do |letter|
+    if letter == " "
+      letter = letter
+    elsif letter == "a" 
+      letter = "e"
+    elsif letter == "e"
+      letter = "i"
+    elsif letter == "i"
+      letter = "o"
+    elsif letter == "o"
+      letter = "u"
+    elsif letter == "u"
+      letter = "a"
+    elsif letter == "d"
+      letter = "f"
+    elsif letter == "h"
+      letter = "j"
+    elsif letter == "n"
+      letter = "p"
+    elsif letter == "t"
+      letter = "v"
+    elsif letter == "z"
+      letter = "b"
+    else
+      letter.next!
+    end
+  end
+  new_alias = name_letters.join.split(' ')
+  new_alias.reverse!
+  new_alias.map! {|name| name.capitalize!}
+  new_alias.insert(1, " ")
+  new_alias = new_alias.join
+  p new_alias   
+end
+
+# make_alias("Molly Fitzharris")
+# make_alias("Felicia Torres")
+

ec765c2f71adc7a199aab5d4f8ba5e29dfcdd731
mfitzharris <molly.fitzharris@gmail.com>
2016-08-16 16:19:41 -0500

    Create Puppy class with multiple methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,30 @@ class Puppy
     toy
   end
 
+  def speak(int)
+    int.times do 
+      puts "Woof"
+    end
+  end
+
+  def roll_over
+    puts "rolls over"
+  end
+
+  def dog_years(human_years)
+    dog_years = human_years * 10
+    dog_years
+  end
+
+  def bark
+    puts "bow wow"
+  end
 end
 
+
+Fido = Puppy.new
+Fido.fetch("ball")
+Fido.speak(5)
+Fido.roll_over
+p Fido.dog_years(3)
+Fido.bark

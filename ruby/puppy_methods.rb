3a61bc924a7b202bb4cbbc04e7791d017eca046d
mfitzharris <molly.fitzharris@gmail.com>
2016-08-16 17:36:39 -0500

    Create Cat class with mutliple methods. Create multiple(50) instances of Cat class. Iterate over all Cat instances to run Cat methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -30,9 +30,52 @@ class Puppy
 end
 
 
+class Cat
+
+  def initialize
+    puts "Initializing new cat instance ..."
+  end
+
+  def run
+    puts "The kitten is running"
+  end
+
+  def meow(int)
+    int.times do
+      puts "Meow"
+    end
+  end
+
+  def toys(int)
+    puts "The kitten has #{int} toys."
+  end
+
+end
+
+
 Fido = Puppy.new
 Fido.fetch("ball")
 Fido.speak(5)
 Fido.roll_over
 p Fido.dog_years(3)
 Fido.bark
+
+all_cats = []
+
+50.times do |int|
+  all_cats << Cat.new
+end
+
+all_cats.each do |cat|
+  cat.run
+  cat.meow(3)
+  cat.toys(5)
+end
+
+
+
+
+
+
+
+

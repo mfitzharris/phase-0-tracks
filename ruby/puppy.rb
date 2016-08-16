73296c238ec0d90b9cd755ed1eb1f3262c2d079e
mfitzharris <molly.fitzharris@gmail.com>
2016-08-16 09:39:04 -0500

    Design a Puppy class, with sections on species, characteristics and behavior

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+#SPECIES------------------
+#puppy
+
+#CHARACTERISTICS----------
+#cute: yes
+#name: varies
+#fur color: varies
+#breed: varies
+#loves walks: yes
+
+#BEHAVIOR-----------------
+#wiggle
+#bark
+#snuggles
+#silly

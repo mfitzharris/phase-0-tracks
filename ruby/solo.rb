46c2c611db548d59630adc2ce1bc7d1058aefda0
mfitzharris <molly.fitzharris@gmail.com>
2016-08-17 14:02:11 -0500

    Design/pseudocode a fairy godmother class

---
+++ ruby/solo.rb
@@ -0,0 +1,23 @@
+#pseudocode a class w at least 3 attributes, 3 methods
+
+# --------------------------------
+# -----FAIRY GODMOTHER------------
+# --------------------------------
+#
+# - CHARACTERISTICS --------------
+# name: varies (string)
+# good?: varies (boolean)
+# age: immortal (static)
+# hair color: varies (string-->randomized from an array?)
+# 
+# - BEHAVIOR ---------------------
+# abracadabra 
+#   puts abracadbra
+# bippity boppity boo
+#   puts bippity boppity boo
+# become evil
+#   change good? alliance to evil
+# help princess
+#   fairy godmother will save name of inputted princess
+
+

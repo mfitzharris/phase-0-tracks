5a521baf9ba021935e090d3fafe156c192f9e5d7
mfitzharris <molly.fitzharris@gmail.com>
2016-08-17 10:20:45 -0500

    Add attributes to Santa class via the initialize method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,8 +1,12 @@
 #define Santa class with three instance methods
 class Santa
 
-  def initialize
+  def initialize(gender, ethnicity)
     p "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
   def speak
@@ -18,6 +22,6 @@ end
 
 
 #TEST CODE
-Kris_Kringle = Santa.new
+Kris_Kringle = Santa.new("Agender", "Polish")
 Kris_Kringle.speak
 Kris_Kringle.eat_milk_and_cookies("short bread cookie")

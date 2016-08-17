5660081921cee60fe25fa6f4d5e7e8cab2ed27aa
mfitzharris <molly.fitzharris@gmail.com>
2016-08-17 10:39:52 -0500

    Add code to allow initialization of multiple instances of Santa class via user/terminal

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -22,6 +22,24 @@ end
 
 
 #TEST CODE
-Kris_Kringle = Santa.new("Agender", "Polish")
-Kris_Kringle.speak
-Kris_Kringle.eat_milk_and_cookies("short bread cookie")
+# Kris_Kringle = Santa.new("Agender", "Polish")
+# Kris_Kringle.speak
+# Kris_Kringle.eat_milk_and_cookies("short bread cookie")
+
+#multiple initializations
+all_santas = []
+
+#get from santa info from user
+puts "Hello Santa! What is your gender:"
+santa_gender = gets.chomp
+#loop to get multiple santa info
+until santa_gender == "done"
+  puts "What is your ethnicity?"
+  santa_ethnicity = gets.chomp
+  all_santas << Santa.new(santa_gender,santa_ethnicity)
+  puts "Hello Santa! What is your gender? (type 'done' to exit)."
+  santa_gender = gets.chomp
+end
+
+#test that array worked
+p all_santas

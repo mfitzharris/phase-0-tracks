d2cd4462346d87df8cf167eb77beccb26c6cf44a
mfitzharris <molly.fitzharris@gmail.com>
2016-08-17 14:35:48 -0500

    Add attributes and methods to Fairy_Godmother class

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -7,7 +7,7 @@
 # - CHARACTERISTICS --------------
 # name: varies (string)
 # good?: varies (boolean)
-# age: immortal (static)
+# age: 0 (static-just born)
 # hair color: varies (string-->randomized from an array?)
 # 
 # - BEHAVIOR ---------------------
@@ -21,11 +21,50 @@
 #   fairy godmother will save name of inputted princess
 
 class Fairy_Godmother
+  attr_reader :is_good
+  attr_accessor :name, :hair_color, :age
 
-  def initialize
-    puts "A new Fairy Godmother was born from the dew drops on a daisy"
+  def initialize(name, hair_color)
+    puts "~ A new Fairy Godmother was born from the dew drops on a daisy ~"
+    @name = name
+    @hair_color = hair_color
+    @is_good = [true, false].sample
+    @age = 0
+  end
+
+  def abracadabra
+    puts "ABRACADABRA!~*~*~"
+  end
+
+  def bippity
+    puts "Bippity ~ Boppity ~ Boo ~"
+  end
+
+  def help_princess(princess)
+    if @is_good
+      puts "WOOHOO! #{@name} saved the day! #{princess} is sounds and safe!"
+    else
+      puts "UH OH, you asked the wrong Fairy Godmother for help."
+      puts "  #{princess} now has to deal with:"
+      puts "  #{["Eternal sleep induced by needle pricking", "Being a frog", "Being hunted by their evil stepmother"].sample}"
+    end
+  end
+
+  #setter method
+  def become_evil
+    @is_good = false
   end
 
 end
 
-Maleficent = Fairy_Godmother.new
+
+
+##### TEST CODE ###########
+
+Maleficent = Fairy_Godmother.new("Maleficent", "blue")
+p Maleficent
+Maleficent.name = "Dragon Maleficent"
+p Maleficent.name
+Maleficent.become_evil
+Maleficent.help_princess("Aurora")
+

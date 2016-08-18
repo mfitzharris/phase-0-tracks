23de665c22f2a56590f0750c8f517eecfcadd036
mfitzharris <molly.fitzharris@gmail.com>
2016-08-17 19:05:19 -0500

    Pseudocode grocery list program to create new list, add item w quantity to list, remove items, update item quantities, print list

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,29 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # Create an empty hash 
+  # split the inputted string by spaces
+  # pass each split element into the hash
+  # set default quantity
+  # print the list to the console [can you use one of your other methods here?]
+# output: hash --> item:quantity
+
+# Method to add an item to a list
+# input: hash, item name and optional quantity
+# steps: pass in new item and quantity to hash
+# output: updated list with new key/value pair
+
+# Method to remove an item from the list
+# input: hash, item name
+# steps: delete the key with item name
+# output: updated hash
+
+# Method to update the quantity of an item
+# input: hash, item name, new desired quantity
+# steps: use a method to update the value of key with item name
+# output: updated key/value in hash
+
+# Method to print a list and make it look pretty
+# input: the list
+# steps: iterate over list 
+# output: item= quantity

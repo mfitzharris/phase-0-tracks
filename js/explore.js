36d160aef6cfc28df35dc4036d13c8581885e7da
mfitzharris <molly.fitzharris@gmail.com>
2016-08-23 10:13:46 -0500

    Pseudocode a function to reverse a string

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+//input - string as parameter
+//function - 
+//  - set a newstring variable to empty string
+//  - Using the length of the string, iterate over backwards
+//  - Add each letter to the new string
+//output - return the reversed string
+//ex - "hello" should output to "olleh"

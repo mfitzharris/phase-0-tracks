1f57f78a6b99aacf1c3bb28751025505b5d89d3e
mfitzharris <molly.fitzharris@gmail.com>
2016-08-23 13:59:55 -0500

    Create an empty object and write code to assign to pair horses as keys with colors as values

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,3 +3,11 @@ var horses = ["Ed", "Phil", "Yasmina", "Rachel"];
 
 colors.push("Orange");
 horses.push("Emmett");
+
+var colorHorses = {};
+
+for (var i = horses.length - 1; i >= 0; i--) {
+  colorHorses[horses[i]] = colors[i];
+}
+
+console.log(colorHorses);

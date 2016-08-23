faf27d408193a939d92f35ab8f2116219abb41e8
mfitzharris <molly.fitzharris@gmail.com>
2016-08-23 10:34:54 -0500

    Write function to reverse a string. Test with driver code.

--- js/explore.js
+++ js/explore.js
@@ -1,7 +1,26 @@
 //input - string as parameter
 //function - 
 //  - set a newstring variable to empty string
-//  - Using the length of the string, iterate over backwards
+//  - for the length(minus 1 to account for zero indexing) of the string, 
+//  - iterate over backwards
 //  - Add each letter to the new string
 //output - return the reversed string
 //ex - "hello" should output to "olleh"
+
+// function reverse(string) {
+//   newString = '';
+//   for (var i = string.length - 1; i >= 0; i--){} 
+//   newString += string[i];
+//   return newString;
+// };
+
+function reverse (string) {
+  newString = '';
+  for (var i = string.length-1; i > -1; i--){
+    newString += string[i];
+  }
+  return newString;
+}
+
+//driver code
+console.log(reverse("hello"))

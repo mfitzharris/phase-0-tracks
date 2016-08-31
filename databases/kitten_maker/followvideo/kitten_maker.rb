debce426a3644ed114a3a4206e97fbeb50bba403
mfitzharris <molly.fitzharris@gmail.com>
2016-08-31 16:44:33 -0500

    Create program to create 10000 of kittens in a database

--- databases/kitten_maker/followvideo/kitten_maker.rb
+++ databases/kitten_maker/followvideo/kitten_maker.rb
@@ -14,15 +14,40 @@ require 'faker' #--> gem lets you create fake data
 # create SQLite3 database
 # sqlite3 somedatabase.db <-- from terminal, want thru ruby
 db = SQLite3::Database.new("kittens.db") #<-- creates database file
+# db.execute("sql command")
+db.results_as_hash = true
 
 # learn about fancy string delimiters
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS kittens( 
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    age INT
+  )
+SQL
 
 # create a kittens table (if it's not there already)
+db.execute(create_table_cmd)
 
 # add a test kitten
+# db.execute("INSERT INTO kittens (name, age) VALUES ('Bob', 10)")
 
-# explore ORM by retrieving data
+
 
 # add LOOOOTS of kittens
 # so. many. kittens.
 # KittenExplosion
+def create_kitten(db, name, age)
+  db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])
+end
+
+10000.times do
+  create_kitten(db, Faker::Name.name, 0)
+end
+
+# explore ORM by retrieving data
+# kittens = db.execute("SELECT * FROM kittens")
+# puts kittens.class
+# kittens.each do |kitten|
+#   puts "#{kitten ['name']} is #{kitten['age']}"
+# end

406752d8d0441f6cb4696906c895b5abdf8ad3a5
mfitzharris <molly.fitzharris@gmail.com>
2016-09-06 16:47:34 -0500

    Make get method for /students/:id print out more nicely

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -8,23 +8,27 @@ db.results_as_hash = true
 # write a basic GET route
 # add a query parameter
 # GET /
+# localhost:9393/?name=molly&age=25
 get '/' do
   "#{params[:name]} is #{params[:age]} years old."
 end
 
 # write a GET route with
 # route parameters
+# localhost:9393/about/molly
 get '/about/:person' do
   person = params[:person]
   "#{person} is a programmer, and #{person} is learning Sinatra."
 end
 
+# localhost:9393/molly/loves/zuzu
 get '/:person_1/loves/:person_2' do
   "#{params[:person_1]} loves #{params[:person_2]}"
 end
 
 # write a GET route that retrieves
 # all student data
+# localhost:9393/students
 get '/students' do
   students = db.execute("SELECT * FROM students")
   response = ""
@@ -39,8 +43,16 @@ end
 
 # write a GET route that retrieves
 # a particular student
-
+# localhost:9393/students/50
 get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
-  student.to_s
+  response = ""
+  response << "ID: #{student['id']}<br>"
+  response << "Name: #{student['name']}<br>"
+  response << "Age: #{student['age']}<br>"
+  response << "Campus: #{student['campus']}<br>"
+  response
 end
+
+
+
